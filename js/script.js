9df7f4dd0aa8bb166683be07667630e23e8aa0ed
Saidaminxo'ja <saidaminxojam@gmail.com>
2022-07-04 15:58:30 +0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Mahmudxo'jayev's
+
+var creator = "Mahmudxo'jayev Saidaminxo'ja"
+console.log(creator)
+
+var ism = prompt("Ismingiz nima:")
+var familiya = prompt("Familiyangiz:")
+var e_mail = prompt("Elektron pochta manzil:")
+var mashgulot = prompt("Sevimli mashg'ulotingiz:")
+var kasb = prompt("Kelejakdagi kasbingiz:")
+
+alert(`Sizning ismingiz: ${ism} \n Familiyangiz: ${familiya} \n E-mailingiz: ${e_mail} \n Sevimli mashg'ulotingiz: ${mashgulot} \n Kasbingiz: ${kasb}`)
+
+alert("Tanishganimizdan Xursandmiz ☺☺☺")
